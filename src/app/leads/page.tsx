4b32254844d0f762613a3ac21cec931a3b528084
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2024-03-07 02:27:13 +0000

    timezone, UTM params

--- src/app/leads/page.tsx
+++ src/app/leads/page.tsx
@@ -8,10 +8,12 @@ const LeadsPage: React.FC = async () => {
   const leads: string[][] = await fetch(
     "https://admin.amamentaclube.com.br/api/leads"
   ).then((res) => res.json());
+
   const clients: HotmartSales[] = await fetch(
     "https://admin.amamentaclube.com.br/api/hotmart/sales"
   ).then((res) => res.json());
   const clientsEmail = clients.map((client) => client.buyer.email);
+  console.log(JSON.parse(leads[1][8]));
   // console.log(clientsEmail);
   //every lead in leads is an array with 9 elements, I want to add an extra element true or false if the lead is a client if the lead[0] is in clientsEmail
   // function to reduce in 3 hours a date
@@ -22,13 +24,28 @@ const LeadsPage: React.FC = async () => {
       <div className="grid grid-cols-1 gap-4 text-gray-700">
         {leads.map((lead, index) => {
           let bgColor = "bg-white";
+          // check if lead[8] is a valid JSON
+          let leadLocation = { city: "N/A", region_code: "", country_code: "" };
+          try {
+            leadLocation = JSON.parse(lead[8]);
+          } catch (e) {
+            console.log("lead[8] is not a valid JSON");
+          }
 
+          // console.log(leadLocation);
           if (clientsEmail.includes(lead[0].trim())) {
             bgColor = "bg-green-300";
           }
           if (lead[1] == "") {
             return null;
           }
+          const urlParams = new URLSearchParams(lead[6].split("?")[1]);
+
+          const utmSource = urlParams.get("utm_source");
+
+          const utmContent = urlParams.get("utm_content");
+          const utmAdset = urlParams.get("utm_adset");
+
           return (
             <div key={index} className={`${bgColor} rounded-lg shadow-md p-4`}>
               <h2 className="text-lg font-semibold">{lead[2]}</h2>
@@ -39,11 +56,16 @@ const LeadsPage: React.FC = async () => {
               <p className="text-gray-500"> CTA:{" " + lead[5]}</p>
 
               <p className="text-gray-500">
-                {" "}
                 {new Date(lead[4]).toLocaleString("pt-BR", {
                   timeZone: "America/Sao_Paulo",
-                })}
+                })}{" "}
+                {leadLocation.city}
+                {", " +
+                  leadLocation.region_code +
+                  ", " +
+                  leadLocation.country_code}
               </p>
+              <p>{utmSource + ", " + utmAdset + ", " + utmContent}</p>
             </div>
           );
         })}
